e9221ca581e9368750630a1fc33aa20a152c7ac5
Viktor Cherniak <barsetca@gmail.com>
2020-05-02 14:09:49 +0300

    lesson03 refactoring structure + Sprite Background

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,54 +1,43 @@
 package ru.geekbrains.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
-    private Texture img;
-    private Vector2 pos;
-    private Vector2 n;
-    private Vector2 touch;
-    private float deltaV;
 
+    private Texture img;
+    private Texture bg;
+    private Background background;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        n = new Vector2();
-        touch = new Vector2();
+        bg = new Texture("starsky.jpg");
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        if (deltaV >= 1) {
-            pos.add(n);
-            deltaV--;
-        } else {
-            pos.set(touch);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y, 150, 150);
+        background.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         img.dispose();
+        bg.dispose();
         super.dispose();
     }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        Vector2 v = touch.cpy().sub(pos);
-        deltaV = v.len();
-        n = v.nor();
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
 }
